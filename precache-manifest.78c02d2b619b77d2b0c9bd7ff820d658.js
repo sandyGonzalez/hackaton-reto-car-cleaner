b9e67a62958cd7aaa9c8cced135c882724dd07c4
@sandyGonzalez <sandygonzalez.612@gmail.com>
2019-11-21 17:18:04 -0600

    Updates

---
+++ precache-manifest.78c02d2b619b77d2b0c9bd7ff820d658.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c556a1fe438b839315e51d427a336597",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d74a8e694f0ea9f67628",
+    "url": "./static/css/main.78a12490.chunk.css"
+  },
+  {
+    "revision": "4debdb409d1f4ce3b692",
+    "url": "./static/js/2.da933122.chunk.js"
+  },
+  {
+    "revision": "d74a8e694f0ea9f67628",
+    "url": "./static/js/main.d8f988cb.chunk.js"
+  },
+  {
+    "revision": "8f8abe65f7daf660ae1d",
+    "url": "./static/js/runtime-main.02e618b3.js"
+  },
+  {
+    "revision": "c25714544fd4bd3733cdcbf5a078df4e",
+    "url": "./static/media/emile-guillemot-ci7gkM_29wA-unsplash.c2571454.png"
+  }
+]);
