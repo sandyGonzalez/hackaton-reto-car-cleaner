ab815644966a1ddb8a333913487c616fdbc4943b
@sandyGonzalez <sandygonzalez.612@gmail.com>
2019-11-21 11:54:25 -0600

    Agregando formulario de registro para el usuario, agregamos la vista para el registro formulario

---
+++ hackaton-reto-car-cleaner/src/views/RegisterUser/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import FormUser from '../../components/FormUser/index';
+import './style.css';
+
+
+const RegisterUser = () => {
+    
+    return (
+        <section className="register-user">
+            <FormUser/>
+        </section>
+    )
+}
+
+export default RegisterUser
