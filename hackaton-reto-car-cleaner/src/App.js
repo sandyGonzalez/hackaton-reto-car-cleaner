2dd90f643356471cbc7c77a10288116dcdf66bbf
rosario VS <paradisekiss-yaoi@hotmail.com>
2019-11-21 11:53:22 -0600

    uniendo rutas

--- hackaton-reto-car-cleaner/src/App.js
+++ hackaton-reto-car-cleaner/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Welcome from './views/Welcome/index'
+import RegisterEmployee from './views/RegisterEmployee/index'
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Switch>
         {/* <Route exact path = "/" render = {props => <Welcome/>}></Route> */}
         {/* <Route exact path = "/registerUser" render = {props => <RegisterUser/>}></Route> */}
-        {/* <Route exact path = "/registerEmployee" render = {props => <RegisterEmployee/>}></Route> */}
+        {<Route exact path = "/registerEmployee" render = {props => <RegisterEmployee/>}></Route> }
         <Route exact path = "/welcome" render = {props => <Welcome/>}></Route>
       </Switch>
     </Router> 
