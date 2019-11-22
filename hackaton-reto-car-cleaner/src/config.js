8d0eba63d7fcc9f491d4592979c85e5d386e3762
Isela Reyes Perdomo <reyesp.isela@gmail.com>
2019-11-21 22:11:38 -0600

    Creando variables de entorno para credenciales. Agregando librería dotenv

--- hackaton-reto-car-cleaner/src/config.js
+++ hackaton-reto-car-cleaner/src/config.js
@@ -1,13 +1,16 @@
- import * as firebase from 'firebase/firebase-app'
+ import * as firebase from 'firebase/firebase-app';
+
+ import dotenv from "dotenv"
+ dotenv.config()
 
  const firebaseConfig = {
-     apiKey: process.env.API_KEY,
-     authDomain: process.env.AUTH_DOMAIN,
-     databaseURL: process.env.DATABASE_URL,
-     projectId: process.env.PROJECT_ID,
-     storageBucket: process.env.STORAGE_BUCKET,
-     messagingSenderId: process.env.MESSAGING_SENDER_ID,
-     appId: process.env.APP_ID
+     apiKey: process.env.REACT_APP_API_KEY,
+     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+     databaseURL: process.env.REACT_APP_DATABASE_URL,
+     projectId: process.env.REACT_APP_PROJECT_ID,
+     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+     appId: process.env.REACT_APP_APP_ID
  };
 
  firebase.initializeApp(firebaseConfig);
